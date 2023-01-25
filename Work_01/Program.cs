45c8abaafd334d73484510a18110f62857b84259
Aleksandr Burakov <aleks-bur-git@yandex.ru>
2023-01-25 21:29:04 +0300

    Save SolutionDescription.md

--- Work_01/Program.cs
+++ Work_01/Program.cs
@@ -1,1 +1,21 @@
-﻿
+﻿using static System.Console;
+Clear();
+WriteLine("Введите количество рядов : ");
+int row = Convert.ToInt32(ReadLine());
+WriteLine("Введите количество столбцов : ");
+int column = Convert.ToInt32(ReadLine());
+Clear();
+string[,] word = new string[row, column];
+var dict = new Dictionary<int, string>()
+{
+{1, "one"}, {2, "two"}, {3, "oops"},
+{4, "cat"}, {5, "three"}, {6, "four"},
+{7, "doctor"}, {8,"hi"}, {9, "six" },
+{10, "my"}, {11, "away"}, {12, "day" },
+{13, "apple"}, {14, "last"}, {15, "man" },
+{16, "old"}, {17, "home"}, {18, "wo" },
+{19, "car"}, {20, "work"}, {21, "-22"},
+{22, "-5"}, {23, "computer science"}, {24, "44"},
+{25, "word"}, {26, "333"}, {27, "hello"},
+{28, "Russia"}, {29, "Denmark"}, {30, "Kazan"}
+};
