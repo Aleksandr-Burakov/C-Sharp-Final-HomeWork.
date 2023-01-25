917186a1b45959d46ed06c9294390c7b061c7773
Aleksandr Burakov <aleks-bur-git@yandex.ru>
2023-01-26 01:53:07 +0300

    FileArray

--- Work_01/Program.cs
+++ Work_01/Program.cs
@@ -40,3 +40,19 @@ void PrintArray(string[,] array)
         WriteLine();
     }
 }
+
+// Метод Рандомного заполнения Двухмерного массива элементами из словаря.
+
+void FileArray(string[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            {
+                int q = new Random().Next(1, 24);
+                word[i, j] = dict[q];
+            }
+        }
+    }
+}
