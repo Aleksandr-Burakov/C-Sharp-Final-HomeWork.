e72fc275dfadd597e58001adfc31fe1ff727f9f5
Aleksandr Burakov <aleks-bur-git@yandex.ru>
2023-01-26 01:49:57 +0300

    PrintArray

--- Work_01/Program.cs
+++ Work_01/Program.cs
@@ -27,4 +27,16 @@ var dict = new Dictionary<int, string>()
 };
 
 
-// 
+// Метод создания Двухмерного массива.
+
+void PrintArray(string[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Write($"  {word[i, j]}  ");
+        }
+        WriteLine();
+    }
+}
